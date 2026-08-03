e7dc2ae678733b649dc98f897c2c297cb6f2a5c1
Kathiravan V <kathiravan.v.1010@gmail.com>
2026-08-03 23:08:10 +0530

    fix: update vercel rewrites and api/index.js URL prefixing for serverless express routing

--- api/index.js
+++ api/index.js
@@ -3,5 +3,9 @@ const connectDB = require("../backend/src/config/db.js");
 
 module.exports = async (req, res) => {
   await connectDB();
+  if (req.url && !req.url.startsWith("/api")) {
+    req.url = "/api" + (req.url.startsWith("/") ? "" : "/") + req.url;
+  }
   return app(req, res);
 };
+
